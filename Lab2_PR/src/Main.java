9d6825766b560a3c41c5373ca1aa54d76fd25f06
Daniel Gorduz <daniel_gorduz@yahoo.com>
2017-03-28 02:34:47 +0300

    Created 5 threads

--- Lab2_PR/src/Main.java
+++ Lab2_PR/src/Main.java
@@ -9,8 +9,9 @@ public class Main {
         CountDownLatch latch5 = new CountDownLatch(1);
 
 
+
         Thread t1 = new Thread(() -> {
-            System.out.println("This is thread 1");
+            System.out.println("--This is thread 1");
             latch.countDown();
         });
         Thread t2 = new Thread(() -> {
@@ -19,15 +20,49 @@ public class Main {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("This is thread 2");
+            System.out.println("+++This is thread 2");
+            latch_234.countDown();
+        });
+        Thread t3 = new Thread(() -> {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("+++This is thread 3");
+            latch_234.countDown();
+        });
+        Thread t4 = new Thread(() -> {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("+++This is thread 4");
             latch_234.countDown();
         });
 
+        Thread t5 = new Thread(() -> {
+            try {
+                latch_234.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("--This is thread 5");
+            latch5.countDown();
+        });
 
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
 
-        
-        System.out.println("Completed!");
+        try {
+            latch5.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("!!Completed!!");
     }
 }
