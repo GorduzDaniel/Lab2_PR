8f6a9ffb02e1d8161f0f60a267a502d83654b781
Daniel Gorduz <daniel_gorduz@yahoo.com>
2017-03-28 02:32:21 +0300

    Added 2 threads

--- Lab2_PR/src/Main.java
+++ Lab2_PR/src/Main.java
@@ -8,6 +8,25 @@ public class Main {
         CountDownLatch latch_234 = new CountDownLatch(3);
         CountDownLatch latch5 = new CountDownLatch(1);
 
+
+        Thread t1 = new Thread(() -> {
+            System.out.println("This is thread 1");
+            latch.countDown();
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("This is thread 2");
+            latch_234.countDown();
+        });
+
+
+        t1.start();
+        t2.start();
+
         
         System.out.println("Completed!");
     }
